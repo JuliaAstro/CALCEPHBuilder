1bf1a946f59407a85b952838c979f02adfda35a0
Bernard Godard <bgodard@users.noreply.github.com>
2019-01-28 13:46:29 +0000

    Fix build_tarballs() invocation

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -31,4 +31,4 @@ products = prefix -> [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, "libcalceph", sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, "libcalceph", v"3.3.0", sources, script, platforms, products, dependencies)

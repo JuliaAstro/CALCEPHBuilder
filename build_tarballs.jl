8bd7f07b31556baff1b1c4bd19fb14e968a64f4f
bgodard <bgodard@users.noreply.github.com>
2019-01-28 12:54:35 +0000

    update to calceph 3.3.0

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -2,14 +2,14 @@ using BinaryBuilder
 
 # Collection of sources required to build libcalceph
 sources = [
-    "https://www.imcce.fr/content/medias/recherche/equipes/asd/calceph/calceph-3.1.0.tar.gz" =>
-    "aaf43641205af6b2d7633eead72d6948e43b77424a56bc1493462d601509be85",
+    "https://www.imcce.fr/content/medias/recherche/equipes/asd/calceph/calceph-3.3.0.tar.gz" =>
+    "be0ee7e838cbd60bb4a90834a1deb4e657d2d83f8dfe7f34774dc41a7c1cb1b4",
 ]
 
 ￼# Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
-cd calceph-3.1.0/
+cd calceph-3.3.0/
 if [[ ${target} == i686-w64* ]] || [[ ${target} == x86_64-w64* ]]; then
     echo 'LT_INIT([win32-dll])' >> configure.ac
     sed -i '/^libcalceph_la_LDFLAGS/ s/$/ -no-undefined/' src/Makefile.am
